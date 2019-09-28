61b3f44b327a24b292d2dfff1f39ab82650ee03c
DANIELA <danielamiritescu@gmail.com>
2019-09-28 15:29:39 +0100

    added projections

---
+++ src/main/java/com/codeclan/newsapp/NewsAppProject/projections/EmbedJournalistForArticle.java
@@ -0,0 +1,14 @@
+package com.codeclan.newsapp.NewsAppProject.projections;
+
+import com.codeclan.newsapp.NewsAppProject.models.Article;
+import com.codeclan.newsapp.NewsAppProject.models.Journalist;
+import org.springframework.data.rest.core.config.Projection;
+
+@Projection(name = "embedAllForJournalists", types = Article.class)
+public interface EmbedJournalistForArticle {
+    public String getTitle();
+    public int getRating();
+    public String getSummary();
+    public Journalist getJournalist();
+
+}
